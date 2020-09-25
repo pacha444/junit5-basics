ff9e12f4578fb6f36efe6817dfa5aa5a57e25b20
pacha444 <pamoussacisse@gmail.com>
2020-09-25 07:45:37 +0000

    First Commit...

---
+++ src/main/java/io/javabrains/MathsUtils.java
@@ -0,0 +1,25 @@
+package io.javabrains;
+
+public class MathsUtils {
+	
+	public int addition (int a, int b)
+	{
+		return a+b;
+	}
+	public int soustraction (int a, int b)
+	{
+		return a-b;
+	}
+	public int division (int a, int b)
+	{
+		return a/b;
+	}
+	public int multiplication (int a, int b)
+	{
+		return a*b;
+	}
+	public double ComputeCircleArea(double Radius)
+	{
+		return Math.PI*Radius*Radius;
+	}
+}
---
+++ src/test/java/io/javabrains/MathsUtilsTest.java
@@ -0,0 +1,56 @@
+package io.javabrains;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+class MathsUtilsTest {
+	
+	MathsUtils mathsUtils;
+	@BeforeEach
+	public void init ()
+	{
+	 mathsUtils=new MathsUtils();
+	}
+
+	@Test
+	void test() {
+		
+	
+		int expected=2;
+		int actual=mathsUtils.addition(1,1);
+		assertEquals(expected, actual, "The add method should add two numbers");
+	}
+	
+	@Test
+	@DisplayName("multiply method")
+	void testMultiply ()
+	{
+	
+		assertAll(
+				()->assertEquals(4,mathsUtils.multiplication(2, 2)),
+				()->assertEquals(0,mathsUtils.multiplication(2, 0)),
+				()->assertEquals(-2,mathsUtils.multiplication(2, -1))
+				);
+	}
+	
+	@Test
+	void testDivide ()
+	{
+	
+		assertThrows(ArithmeticException.class, ()->mathsUtils.division(1, 0), "Divide by zero should throw");
+	}
+	
+	@Test
+	@Tag("Circle")
+	void testComputeCircleArea() {
+		
+		
+		assertEquals(314,1519, mathsUtils.ComputeCircleArea(10), "Should return right circle area");
+	}
+
+}
